49d4e9d4190dfa27da2693b7debafab7ac33cacd
Радислав Коваленко <you@example.com>
2021-02-08 17:59:01 +0300

    Auto

---
+++ Auto/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Auto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "182-1828911_medium-image-car-pixel-art-top-view.png");
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 25;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 25;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Interval = 25;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Auto.Properties.Resources._1556547969;
+            this.ClientSize = new System.Drawing.Size(800, 478);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Auto/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Auto
+{
+    public partial class Form1 : Form
+    {
+        private int _width = 840;
+        private int _height = 650;
+        Rectangle invalidrect;
+        public int set=0;
+        
+        public Bitmap acar = Resource1.carr;
+        public Bitmap gif = Resource1.transparent_sprite_zombie_6;
+        public bool t = true;
+        public int xx = 180;
+        public int yy = 600;
+        public PointF rect;
+        public List<PointF> list;
+        public PointF _x;
+        public bool x = true;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetStyle(ControlStyles.OptimizedDoubleBuffer | ControlStyles.AllPaintingInWmPaint, true);
+            UpdateStyles();
+            
+            this.Width = _width;
+            this.Height = _height;
+            this.Text = "Auto";
+            _x = new PointF(xx, yy);
+            RandGo();
+            timer1.Interval = 20;
+            timer1.Tick += new EventHandler(update);
+            timer1.Start();
+            this.KeyDown += new KeyEventHandler(OKP);
+            timer3.Interval = 1;
+            timer3.Tick += new EventHandler(Find);
+            timer3.Start();
+
+
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            
+            invalidrect = new Rectangle(invalidrect.X, invalidrect.Y = 280, 100, 100);
+
+
+
+
+        }
+        //Отслеживание столкновений и счетчик удачных переходов через дорогу
+        private void Find(object sender, EventArgs e)
+        {
+            if (Math.Abs(invalidrect.Y - rect.Y) <= 50)
+            {
+                if (Math.Abs(invalidrect.X - rect.X) <= 50)
+                {
+                    MessageBox.Show("Collide!!!!");
+                    DestroyHandle();
+                }
+            }
+            if (invalidrect.X > 800)
+            {
+                invalidrect.X = -50;
+                set += 1;
+                label1.Text = set.ToString();
+                
+
+            }
+        }
+        //Появление и движение машины
+        private void update(object sender, EventArgs e)
+        {
+            
+
+            rect.Y -= 10;
+            if (rect.Y < -100)
+            {
+                
+                rect = list[new Random().Next(0, list.Count)];
+                rect.Y = 600;
+            }
+            Invalidate();
+        }
+        //Создание списка PoinF для рандомного появления машины 
+        public void RandGo()
+        {
+            
+            
+            list = new List<PointF>();
+            
+            if (t == true)
+            {
+                t = false;
+                for (int i = 0; i < 4; i++)
+                {
+                    list.Add(_x);
+                    xx += 125;
+                    _x = new PointF(xx, yy);
+
+                }
+            }
+            
+            
+            
+            
+        }
+        
+        private void OnFrameChanged(object o,EventArgs e)
+        {
+            this.Invalidate(invalidrect);
+        }
+        //Анимация
+        private void Form1_Load(object sender, EventArgs e)
+            {
+            if (ImageAnimator.CanAnimate(gif))
+            {
+                ImageAnimator.Animate(gif, new EventHandler(this.OnFrameChanged));
+            }
+            }
+        //Прорисовка
+        public void  Form1_Paint(object sender, PaintEventArgs e)
+            {
+                Graphics g = e.Graphics;
+                
+                g.DrawImage(acar, new Rectangle((int)rect.X,(int)rect.Y,100,100));
+                ImageAnimator.UpdateFrames(gif);
+                g.DrawImage(gif, invalidrect);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+            {
+                this.Refresh();
+            }
+       
+   //Управление 
+        private void OKP(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode.ToString())
+            {
+                case "Right":
+                    {
+                        invalidrect.X += 10;
+                }
+
+
+                    break;
+                case "Left":
+                    {
+
+                        invalidrect.X -= 10;
+                      
+                    }
+                    break;
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            this.Refresh();
+        }
+    }
+
+    
+}
